2f28029c4b25fa79cbcd044ab8848fef07f5887f
Ritu Agrawal <rituagrawal@Ritus-MacBook-Air.local>
2021-11-07 14:17:16 -0600

    Implemented React Redux

---
+++ src/redux/cake/cakeActions.js
@@ -0,0 +1,8 @@
+import { BUY_CAKE } from './caketypes'
+
+//Action Creator
+export const buyCake = () => {
+    return {
+            type : BUY_CAKE
+        }
+}
---
+++ src/redux/cake/caketypes.js
@@ -0,0 +1,2 @@
+//Action constants
+export const BUY_CAKE = 'BUY_CAKE';
